2fe6476aed440e49a5dddb83737f31a4d3130286
JorgeSalazarMora <jorgesalazarmorag@gmail.com>
2016-01-30 10:01:47 -0600

    Hola Semana 2!!

---
+++ Hello.Word.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hello.Word.Tests.Client;
+
+namespace Hello.Word.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSayHelloMyName()
+        {
+            // preparar el escenario
+            string myName = "Jorge";
+            string resultadoEsperado = "Hello world, Jorge";
+            string resultadoReal;
+            HelloWorldClient client = new HelloWorldClient();
+
+            // invoco al metodo correspondiente
+            resultadoReal = client.SayHello(myName);
+
+
+            // comparacion de resultados
+            Assert.AreEqual(resultadoEsperado, resultadoReal);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
